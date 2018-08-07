8e264e5096009ed66c2410da4d689d9ffa9ee318
Tobias Knöschke <tobias.knoeschke@student.hpi.uni-potsdam.de>
2018-08-07 11:50:24 +0200

    Update master documentation
      current state: cpplocate commit 8b7df80

--- docs/master/search/functions_1.js
+++ docs/master/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['locatepath',['locatePath',['../namespacecpplocate.html#ab9d6d4b2d08682c3233552bca74f6a42',1,'cpplocate']]]
+  ['locatepath',['locatePath',['../namespacecpplocate.html#ab9d6d4b2d08682c3233552bca74f6a42',1,'cpplocate::locatePath()'],['../liblocate_8h.html#a9d8a8b584c9353f41b56dbbebe493bf8',1,'locatePath():&#160;liblocate.h']]]
 ];

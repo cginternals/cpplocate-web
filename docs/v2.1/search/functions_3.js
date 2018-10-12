a53b55df667ffbe84d7fd8c67c8a7a0bd6a38d5a
Willy Scheibel <willy.scheibel@hpi.de>
2018-10-12 17:38:09 +0200

    Update docs

---
+++ docs/v2.1/search/functions_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['libextension',['libExtension',['../namespacecpplocate.html#a58b1f33bd79f7604085ddb8f76b7a900',1,'cpplocate::libExtension()'],['../liblocate_8h.html#a0e7cfc0cfedd8c356866fed58c072065',1,'libExtension():&#160;liblocate.h']]],
+  ['libprefix',['libPrefix',['../namespacecpplocate.html#ab33976d9e52ec15cf1fa082e1477e40b',1,'cpplocate::libPrefix()'],['../liblocate_8h.html#a9cbb66f83a7bc6c3bb84f8d62cb69191',1,'libPrefix():&#160;liblocate.h']]],
+  ['locatepath',['locatePath',['../namespacecpplocate.html#ab9d6d4b2d08682c3233552bca74f6a42',1,'cpplocate::locatePath()'],['../liblocate_8h.html#a9d8a8b584c9353f41b56dbbebe493bf8',1,'locatePath():&#160;liblocate.h']]]
+];
---
+++ docs/v2.1/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['pathseparator',['pathSeparator',['../namespacecpplocate.html#a60b4bd1b7d7e58fb119f2ba8387446d9',1,'cpplocate::pathSeparator()'],['../liblocate_8h.html#a4cac69129ac0670d6be52cc290ab87bb',1,'pathSeparator():&#160;liblocate.h']]]
+];
